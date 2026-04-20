b2e7646184440f499a83df7cdd9422cb578ec790
Guilherme Holanda <guilherme.holanda@sap.com>
2026-04-20 16:21:26 -0300

    feat: add SessionProvider to app providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,23 +1,21 @@
-"use client";
+'use client';
 
-import type { ThemeProviderProps } from "next-themes";
-
-import * as React from "react";
-import { HeroUIProvider } from "@heroui/system";
-import { useRouter } from "next/navigation";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import {ToastProvider} from "@heroui/toast";
+import type { ThemeProviderProps } from 'next-themes';
+import * as React from 'react';
+import { HeroUIProvider } from '@heroui/system';
+import { useRouter } from 'next/navigation';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ToastProvider } from '@heroui/toast';
+import { SessionProvider } from 'next-auth/react';
 
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
 }
 
-declare module "@react-types/shared" {
+declare module '@react-types/shared' {
   interface RouterConfig {
-    routerOptions: NonNullable<
-      Parameters<ReturnType<typeof useRouter>["push"]>[1]
-    >;
+    routerOptions: NonNullable<Parameters<ReturnType<typeof useRouter>['push']>[1]>;
   }
 }
 
@@ -25,9 +23,11 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <HeroUIProvider navigate={router.push}>
-      <ToastProvider />
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-    </HeroUIProvider>
+    <SessionProvider>
+      <HeroUIProvider navigate={router.push}>
+        <ToastProvider />
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </HeroUIProvider>
+    </SessionProvider>
   );
 }
