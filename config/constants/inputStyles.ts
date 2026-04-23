36ccb001f3b8f1b431242a21bedc83fc973547d0
Guilherme Holanda <guilherme.holanda@sap.com>
2026-04-23 08:41:34 -0300

    fix: remove top margin from input base slot

--- config/constants/inputStyles.ts
+++ config/constants/inputStyles.ts
@@ -4,6 +4,7 @@
 export const inputLabelClass = 'text-xs font-normal text-default-400';
 
 export const borderedInputClassNames = {
+  base: 'mt-0',
   label: inputLabelClass,
   inputWrapper: [
     'border-white/[0.14]',
