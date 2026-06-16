9f34e87f77ab75ef8a54384f2e3001173b603ed8
Guilherme Holanda <guilherme.holanda@sap.com>
2026-06-16 12:10:28 -0300

    fix: use named prisma import in MockExamService

--- app/api/mock-exams/mock-exam.service.ts
+++ app/api/mock-exams/mock-exam.service.ts
@@ -1,4 +1,4 @@
-import prisma from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 import { CreateMockExamPayload, MockExamSubjectConfig } from '@/shared/types';
 
 export class MockExamService {
