87b4840513470b0fe4612ee1fb3300679a605c5b
Guilherme Holanda <guilherme.holanda@sap.com>
2026-08-01 12:46:48 -0300

    feat: add metadataBase, OG/Twitter metadata and fix html lang to pt-BR

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,32 @@ import { fontSans, fontSora } from '@/config/fonts';
 import { parseProperties } from '@/lib/properties-parser';
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
+  metadataBase: new URL('https://www.certifiqueai.com'),
+  title: siteConfig.title,
   description: siteConfig.description,
+  openGraph: {
+    type: 'website',
+    siteName: 'CertifiqueAI',
+    url: 'https://www.certifiqueai.com',
+    locale: 'pt_BR',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [{ url: '/og-image.png', width: 1200, height: 630, alt: 'CertifiqueAI' }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: ['/og-image.png'],
+  },
+  alternates: {
+    canonical: 'https://www.certifiqueai.com',
+    languages: {
+      'pt-BR': 'https://www.certifiqueai.com',
+      en: 'https://www.certifiqueai.com',
+      'x-default': 'https://www.certifiqueai.com',
+    },
+  },
 };
 
 export const viewport: Viewport = {
@@ -41,7 +62,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   const [initialMessages, session] = await Promise.all([loadDefaultMessages(), auth()]);
 
   return (
-    <html suppressHydrationWarning lang="en">
+    <html suppressHydrationWarning lang="pt-BR">
       <head />
       <body
         className={clsx(
