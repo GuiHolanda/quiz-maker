f1b1576636511f29e32c25f18892c1b7cd116ddf
Guilherme Holanda <guilherme.holanda@sap.com>
2026-08-01 12:54:34 -0300

    chore: add display swap to all Google Font configs

--- config/fonts.ts
+++ config/fonts.ts
@@ -3,15 +3,18 @@ import { Fira_Code as FontMono, Inter as FontSans, Sora as FontSora } from 'next
 export const fontSans = FontSans({
   subsets: ['latin', 'latin-ext'],
   variable: '--font-sans',
+  display: 'swap',
 });
 
 export const fontMono = FontMono({
   subsets: ['latin'],
   variable: '--font-mono',
+  display: 'swap',
 });
 
 export const fontSora = FontSora({
   subsets: ['latin'],
   variable: '--font-sora',
   weight: ['300', '400', '500', '600', '700', '800'],
+  display: 'swap',
 });
